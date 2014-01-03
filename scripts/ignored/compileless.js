090cb17f1ceff7a8c830c569e64c9be82097865a
Alexey Lukin <alukin@qarea.us>
2014-01-03 09:06:08 -0500

    add compile option param

--- scripts/ignored/compileless.js
+++ scripts/ignored/compileless.js
@@ -1,8 +1,12 @@
 var exec = require('child_process').exec;
 var path = process.argv[2];
 var files = process.argv[3].split(',');
+var env = process.argv[4];
+if (!env) env = 'dev';
 files.forEach(function(file){
-	console.log('node_modules/less/bin/lessc --line-numbers=mediaquery '+path+'/less/'+file+'.less > '+path+'/'+file+'.css');
-	exec('node_modules/less/bin/lessc --line-numbers=mediaquery '+path+'/less/'+file+'.less > '+path+'/'+file+'.css');
-})
+	if (env == 'dev')
+		exec('node_modules/less/bin/lessc --line-numbers=mediaquery '+path+'/less/'+file+'.less > '+path+'/'+file+'.css');
+	else
+		exec('node_modules/less/bin/lessc -x '+path+'/less/'+file+'.less > '+path+'/'+file+'.css');
 
+})
