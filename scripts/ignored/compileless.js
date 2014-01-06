ce197433ad5aa4dcd71c3e74639ecbfcc795f3d4
Alexey Lukin <alukin@qarea.us>
2014-01-06 09:51:36 -0500

    Update compileless.js
    check less version

--- scripts/ignored/compileless.js
+++ scripts/ignored/compileless.js
@@ -1,12 +1,31 @@
-var exec = require('child_process').exec;
-var path = process.argv[2];
-var files = process.argv[3].split(',');
-var env = process.argv[4];
-if (!env) env = 'dev';
-files.forEach(function(file){
-	if (env == 'dev')
-		exec('node_modules/less/bin/lessc --line-numbers=mediaquery '+path+'/less/'+file+'.less > '+path+'/'+file+'.css');
-	else
-		exec('node_modules/less/bin/lessc -x '+path+'/less/'+file+'.less > '+path+'/'+file+'.css');
+#!/usr/bin/nodejs
 
+var exec = require('child_process').exec;
+
+var server = process.argv[2];
+var path   = process.argv[3];
+var files  = process.argv[4].split(',');
+
+var lessc  = 'node_modules/less/bin/lessc';
+//test if version match
+exec(lessc + ' -v',
+  function (error, stdout, stderr) {
+    var version = stdout.match('[0-9\.]+')[0];
+    if (version < '1.5.0') lessc = '/usr/local/lib/node_modules/less/bin/lessc';
+console.log(version, lessc)
+var env = process.argv[5];
+if (!env) env = 'dev';
+var params;
+if (env == 'dev') {
+  params = ' --line-numbers=all'
+         + ' --source-map-rootpath=http://'+server;
+} else {
+  params = ' -x';
+}
+
+files.forEach(function(file){
+  if (env == 'dev') params += ' --source-map='+path+'/'+file+'.css.map';
+  exec( lessc + params + ' ' + path+'/less/'+file+'.less ' + path+'/'+file+'.css');
 })
+
+});
