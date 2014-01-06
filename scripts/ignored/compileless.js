f454fe004d15b45e1b01b92db21d0ab6583146fd
Alexey Lukin <alukin@qarea.us>
2014-01-06 09:52:44 -0500

    Update compileless.js

--- scripts/ignored/compileless.js
+++ scripts/ignored/compileless.js
@@ -12,20 +12,18 @@ exec(lessc + ' -v',
   function (error, stdout, stderr) {
     var version = stdout.match('[0-9\.]+')[0];
     if (version < '1.5.0') lessc = '/usr/local/lib/node_modules/less/bin/lessc';
-console.log(version, lessc)
-var env = process.argv[5];
-if (!env) env = 'dev';
-var params;
-if (env == 'dev') {
-  params = ' --line-numbers=all'
-         + ' --source-map-rootpath=http://'+server;
-} else {
-  params = ' -x';
-}
+    var env = process.argv[5];
+    if (!env) env = 'dev';
+    var params;
+    if (env == 'dev') {
+      params = ' --line-numbers=all'
+             + ' --source-map-rootpath=http://'+server;
+      } else {
+        params = ' -x';
+      }
 
-files.forEach(function(file){
-  if (env == 'dev') params += ' --source-map='+path+'/'+file+'.css.map';
-  exec( lessc + params + ' ' + path+'/less/'+file+'.less ' + path+'/'+file+'.css');
-})
-
+      files.forEach(function(file){
+        if (env == 'dev') params += ' --source-map='+path+'/'+file+'.css.map';
+        exec( lessc + params + ' ' + path+'/less/'+file+'.less ' + path+'/'+file+'.css');
+      })
 });
